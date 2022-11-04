48d6f2576b5937fbd2c6b68dde6fa20b98fa9b64
BeckyK <barimonsterbk@gmail.com>
2022-11-04 15:46:04 -0500

    pybank code updates, totals working, increase/decrease debatable

---
+++ PyBank/main.py
@@ -0,0 +1,71 @@
+import os
+import csv
+
+#path to the report
+csvpath = os.path.join('Resources', 'budget_data.csv')
+
+# The total number of months included in the dataset
+total_months = 0
+# The net total amount of "Profit/Losses" over the entire period
+net_total = 0
+# The changes in "Profit/Losses" over the entire period, and then the average of those changes
+average_change = 0.0  # hmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm
+# The greatest increase in profits over the entire period
+greatest_increase = 0
+# The greatest decrease in profits over the entire period
+greatest_decrease = 0
+
+#open the csv report
+with open(csvpath, 'r') as csvfile:
+
+        #open a csv reader
+        csvreader = csv.reader(csvfile, delimiter=',')
+
+        #store the header in a variable for later
+        header = next(csvreader)
+
+        #iterate through each row in the report
+        for row in csvreader:
+
+            #count the total months in the report
+            total_months += 1
+
+            #create a variable to cast the profit/loss column value to an integer for simplicity
+            r = int(row[1])
+            
+            #add the profit/loss to a running total
+            net_total += r
+
+            #check profit/loss against greatest increase/decrease and update date & amount as found
+            if r > greatest_increase:
+                greatest_increase = r
+                gi_month = row[0]
+            
+            if r < greatest_decrease:
+                greatest_decrease = r
+                gd_month = row[0]
+
+            
+        
+
+
+
+
+print(total_months, net_total, gi_month, greatest_increase, gd_month, greatest_decrease)
+
+
+
+ #```text
+ # Financial Analysis
+ # ----------------------------
+ # Total Months: 86
+ # Total: $22564198
+ # Average Change: $-8311.11
+ # Greatest Increase in Profits: Aug-16 ($1862002)
+ # Greatest Decrease in Profits: Feb-14 ($-1825558)
+ # ```
+#In addition, your final script should both print the analysis to the terminal and export a text file with the results.
+
+#Consider what you've learned so far. You've learned how to import modules like `csv`; to read and write files in various formats; 
+ #to store contents in variables, lists, and dictionaries; to iterate through basic data structures; and to debug along the way. 
+ #Using what you've learned, try to break down your tasks into discrete mini-objectives.
---
+++ PyPoll/main.py
@@ -0,0 +1,31 @@
+
+
+
+# The total number of votes cast
+
+# A complete list of candidates who received votes
+
+# The percentage of votes each candidate won
+
+# The total number of votes each candidate won
+
+# The winner of the election based on popular vote.
+
+ # ```text
+ # Election Results
+ # -------------------------
+ # Total Votes: 369711
+ # -------------------------
+ # Charles Casper Stockham: 23.049% (85213)
+ # Diana DeGette: 73.812% (272892)
+ # Raymon Anthony Doane: 3.139% (11606)
+ # -------------------------
+ # Winner: Diana DeGette
+ # -------------------------
+ # ```
+
+ #In addition, your final script should both print the analysis to the terminal and export a text file with the results.
+
+ #Consider what you've learned so far. You've learned how to import modules like `csv`; to read and write files in various formats; 
+ #to store contents in variables, lists, and dictionaries; to iterate through basic data structures; and to debug along the way. 
+ #Using what you've learned, try to break down your tasks into discrete mini-objectives.
