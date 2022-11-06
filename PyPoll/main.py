4ded12ea70e681739a8c7365bcec1d2776e1ed0d
BeckyK <barimonsterbk@gmail.com>
2022-11-06 10:06:26 -0600

    formatting and efficiency tweaks

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,7 +1,8 @@
+# import required modules
 import os
 import csv
 
-#path to the election data
+# path to the election data
 csvpath = os.path.join('Resources', 'election_data.csv')
 
 # The total number of votes cast
@@ -14,69 +15,65 @@ count = {}
 x = 0
 winner = ''
 
-#open the csv report
+# open the csv report
 with open(csvpath, 'r') as csvfile:
 
-    #open a csv reader
+    # open a csv reader
     csvreader = csv.reader(csvfile, delimiter=',')
 
-    #store the report header
+    # store the report header
     header = next(csvreader)
 
-    #iterate through each row in the report
+    # iterate through each row in the report
     for row in csvreader:
 
-        #running count of total votes
+        # running count of total votes
         total_votes += 1
 
-        #check if candidate voted for is included in our list yet, if not, add
+        # check if candidate voted for is included in our list yet, if not, add
         if row[2] not in candidates:
             candidates.append(row[2])
 
-            #initialize candidate vote count
+            # initialize candidate vote count
             count[row[2]] = 0
             
-        #add each vote to each candidate's total
+        # add each vote to each candidate's total
         count[row[2]] += 1
 
-    #iterate through the final count
+    # iterate through the final count
     for k, v in count.items():
 
-        #check each candidate against the next for the highest count to get the winner
+        # check each candidate against the next for the highest count to get the winner
         if v > x:
             winner = k
             x = v
 
-        #convert the counts for each candidate into percentages and store along with their name/count
-        count[k] = [k, v, round(v/total_votes*100, 3)]
+        # convert the counts for each candidate into percentages and store along with their name/count
+        count[k] = [round(v/total_votes*100, 3), v]
 
 
-#print the final results to terminal
-print('---')
-print('Election Results')
+# print the final results to terminal
+print('\nElection Results')
 print('---------------------------')
 print('Total Votes: {}'.format(total_votes))
 print('---------------------------')
 for k in count:
-    print('{}: {}% ({})'.format(count[k][0], count[k][2], count[k][1]))
+    print('{}: {}% ({})'.format(k, count[k][0], count[k][1]))
 print('---------------------------')
 print('Winner: {}'.format(winner))
-print('---------------------------')
-print('---')
+print('---------------------------\n')
 
-#path to create finished report
+# path to create finished report
 txtpath = os.path.join('Analysis','Election_Results.txt')
 
-#open new file to print finished report to text file
+# open new file to print finished report to text file
 with open(txtpath, 'w') as textfile:
-    textfile.write('---\n')
-    textfile.write('Election Results\n')
+    textfile.write('\nElection Results\n')
     textfile.write('---------------------------\n')
     textfile.write('Total Votes: {}\n'.format(total_votes))
     textfile.write('---------------------------\n')
     for k in count:
-        textfile.write('{}: {}% ({})\n'.format(count[k][0], count[k][2], count[k][1]))
+        textfile.write('{}: {}% ({})\n'.format(k, count[k][0], count[k][1]))
     textfile.write('---------------------------\n')
     textfile.write('Winner: {}\n'.format(winner))
     textfile.write('---------------------------\n')
-    textfile.write('---')
